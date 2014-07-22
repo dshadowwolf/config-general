5ad0bbf4ab4e0f042654c794d00c3c57ce75325e
Daniel Hazelton <dshadowwolf@gmail.com>
2014-07-22 05:44:07 -0400

    test number 50 in the original test-suite converted and system passes

---
+++ t/26-test-multiple-quoted-strings-in-a-value.js
@@ -0,0 +1,20 @@
+var tap = require('tap'),
+    plan = tap.plan,
+    test = tap.test,
+    parser = require('../index');
+
+var dupcount50 = 2000;
+var ostr50 = "";
+
+for( var i = dupcount50; i > 0; i-- ) {
+  ostr50 = ostr50.concat(" 'luck"+i+"'");
+}
+ostr50 = ostr50.trimLeft();
+var cfgsrc50 = 'test_single_many '+ostr50;
+var cfg50 = new parser.parser( { String: cfgsrc50, InterPolateVars: true } );
+var hash50 = cfg50.getall();
+test("test a value consisting of a lot of single-quoted strings", function(t) {
+  t.plan(1);
+  t.is( hash50.test_single_many, ostr50, "value with single-quote strings is as expected" );
+  t.end();
+});
