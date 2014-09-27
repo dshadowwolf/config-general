5523f2d51c3a98a87a77d587f8ffd6bde02289fd
Jonathan Hall <jhall@efolder.net>
2014-09-26 19:39:02 -0500

    Add a new test which fails.

--- t/33-generate-flist.js
+++ t/33-generate-flist.js
@@ -2,32 +2,32 @@ var tap = require('tap'),
     plan = tap.plan,
     test = tap.test,
     parser = require('../index'),
-    cfgparser = require('../lib/cg_parser.js');
+    parser = require('../lib/cg_parser');
+//var foo = new cfgparser({});
+//console.warn( foo ) ;
+//console.warn(cfgparser.generate_flist( { pattern: 'included.conf'} ) );
 // var console = require('console'),
 //     util = require('util');
 
-var conf47 = new parser.parser( {
-                 ConfigFile: "complex.cfg",
-                 InterPolateVars: true,
-                 DefaultConfig: { this: "that", default: "imported" },
-                 MergeDuplicateBlocks: true,
-                 MergeDuplicateOptions: true,
-                 StrictVars: true,
-                 SplitPolicy: 'custom',
-                 SplitDelimiter: '\\s*=\\s*',
-                 IncludeGlob: true,
-                 IncludeAgain: true,
-                 IncludeRelative: true,
-                 AutoTrue: true,
-                 FlagBits: { someflags: { LOCK: 1, RW: 2, TAINT: 3 } },
-                 StoreDelimiter: ' = ',
-                 SlashIsDirectory: true,
-                 SaveSorted: true
-            } );
 
-test("flist generation", function(t) {
+test("Basic flist generation", function(t) {
     t.plan(1);
-    t.is(1,1,"One!");
-//    t.isDeeply( cfgparser.generate_flist({ pattern: 'included.conf' }), {}, "flist 1");
+    var conf = new parser( { ConfigFile: "cfg.33" } );
+    t.isDeeply( conf.generate_flist({ pattern: 'included.conf' }), ["included.conf"], "basic flist");
+    t.end();
+});
+
+test("Complex flist generation", function(t) {
+    t.plan(1);
+    var conf = new parser( {
+        ConfigFile:             "../t/cfg.33",
+        ConfigPath:             [ "../t/" ],
+        IncludeDirectories:     true,
+        IncludeRelative:        true,
+        IncludeGlob:            true,
+        MergeDuplicateBlocks:   true,
+        InterPolateVars:        true
+    });
+    t.isDeeply( conf.generate_flist({ pattern: 'included.conf' }), ["../t/included.conf"], "complex flist");
     t.end();
 });
